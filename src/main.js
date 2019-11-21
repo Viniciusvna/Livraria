6e6eee1aedb94b62a4bdb95af104c5a6c47c1787
Necs <icaroibernon@live.com>
2019-11-21 01:03:41 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.vue 	deleted:    src/components/HelloWorld.vue 	new file:   src/components/call.js 	modified:   src/main.js 	new file:   src/pages/Clientes.vue 	new file:   src/pages/Livros.vue 	new file:   src/store/index.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from "vue-router"
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import Clientes from "@/pages/Clientes.vue"
+import Livros from "@/pages/Livros.vue"
 
 Vue.config.productionTip = false
 
+Vue.use(VueMaterial)
+
+Vue.use(Router)
+
+const routes = [
+  { path: '@/pages/Clientes.vue', component: Clientes },
+  { path: '@/pages/Livros.vue', component: Livros }
+]
+
 new Vue({
-  render: h => h(App),
+  routes,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/pages/Clientes.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul style="list-style-type:none">
+    <li >
+      <md-card v-for="post of posts" :key="post">
+        <md-card-header>
+          <div class="md-title"><p><strong>{{post.title}}</strong></p></div>
+        </md-card-header>
+        <md-card-content><p>{{post.body}}</p></md-card-content>
+        <md-card-actions>
+          <md-button>Alterar</md-button>
+          <md-button>Deletar</md-button>
+        </md-card-actions>
+      </md-card>
+    </li>
+  </ul>
+</template>
+
+
+<script>
+import {HTTP} from '@/components/call.js';
+
+export default {
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  created() {
+    HTTP.get('posts')
+    .then(response => {
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .md-card {
+    width: 400px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+  }
+</style>
